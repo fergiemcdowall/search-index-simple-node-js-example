f1713acb509da9abbff36acf9afe0aea331e1727
fergiemcdowall <fergusmcdowall@gmail.com>
2015-09-02 16:42:11 +0200

    NOW working

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var should = require('should');
 
 describe('simple search', function() {
   it('can index and search', function(done) {
-    var prog = require('../main.js');
+    var prog = require('../lib/main.js');
     prog.addAndSearch(function(err, results){
       (err === null).should.be.exactly(true);
       results.hits.length.should.be.exactly(9);
